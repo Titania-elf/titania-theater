9ca6dae3ba96bbe793fae2aa5fef74664ec5109b
Titania-elf <xw5397@outlook.com>
2025-12-12 15:04:10 +0800

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+    "display_name": "Titania's Little Theater",
+    "author": "Titania Dev",
+    "version": "3.0.0",
+    "loading_order": 100,
+    "requires": [],
+    "css": "style.css",
+    "js": "index.js"
+}
